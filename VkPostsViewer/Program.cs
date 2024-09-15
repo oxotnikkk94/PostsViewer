a7dcceb24879be313635c535578571d89fcce52a
Evgeniy Popov <jenya051994@yandex.ru>
2024-09-15 22:46:24 +0500

    TelegaPars

--- VkPostsViewer/Program.cs
+++ VkPostsViewer/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -19,14 +23,17 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "parsing",
+    pattern: "parsing",
+    defaults: new { controller = "Telegram", action = "Index" });
 
 
 app.Run();
